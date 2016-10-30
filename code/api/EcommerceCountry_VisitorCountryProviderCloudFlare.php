b1757101538d19d74e6853533773f7f60e3cd7c1
Sunny Side Up <github@sunnysideup.co.nz>
2016-10-31 11:55:31 +1300

    PATCH: cleanup of code

--- code/api/EcommerceCountry_VisitorCountryProviderCloudFlare.php
+++ code/api/EcommerceCountry_VisitorCountryProviderCloudFlare.php
@@ -3,17 +3,24 @@
 /**
  * this is a very basic class with as its sole purpose providing
  * the country of the customer.
- * By default we are using the GEOIP class
- * but you can switch it to your own system by changing
- * the classname in the ecommerce.yml config file.
  *
  *
  */
 class EcommerceCountry_VisitorCountryProviderCloudFlare extends Object implements EcommerceGEOipProvider
 {
+
+    /**
+     *
+     * @var string
+     */
     private static $forced_country_code = "";
 
+    /**
+     *
+     * @var string
+     */
     private static $forced_ip_address = "";
+
     /**
      *
      * @return String (Country Code - e.g. NZ, AU, or AF)
@@ -26,6 +33,11 @@ class EcommerceCountry_VisitorCountryProviderCloudFlare extends Object implement
         return CloudFlareGeoip::visitor_country();
     }
 
+    /**
+     *
+     *
+     * @return string
+     */
     public function getIP()
     {
         if ($ip = Config::inst()->get("EcommerceCountry_VisitorCountryProviderCloudFlare", "forced_ip_address")) {
